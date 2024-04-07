b8231cc1217dc65786328a5b63b4b5471197b4d2
baguzbw <bagusbwicaksono5@gmail.com>
2024-04-08 01:24:16 +0700

    sidebar props

--- src/components/SideMenu.jsx
+++ src/components/SideMenu.jsx
@@ -1,53 +1,16 @@
+import PropTypes from "prop-types";
 import React, { useState } from "react";
-import { Activity, ChevronDown, ChevronUp, Document, Folder, Home, Logout, TwoUsers } from "react-iconly";
+import { ChevronDown, ChevronUp } from "react-iconly";
 import { useLocation, useNavigate } from "react-router-dom";
 
-export default function SideMenu() {
+function SideMenu({ menu }) {
   const navigate = useNavigate();
   const location = useLocation();
-  const [masterDataOpen, setMasterDataOpen] = useState(false);
-  const [resourceManagerOpen, setResourceManagerOpen] = useState(false);
+  const [openSubMenus, setOpenSubMenus] = useState({});
 
-  const toggleMasterData = (e) => {
-    e.stopPropagation(); // Prevents navigation event
-    setMasterDataOpen(!masterDataOpen);
+  const toggleSubMenu = (name) => {
+    setOpenSubMenus({ ...openSubMenus, [name]: !openSubMenus[name] });
   };
-
-  const toggleResourceManager = (e) => {
-    e.stopPropagation(); // Prevents navigation event
-    setResourceManagerOpen(!resourceManagerOpen);
-  };
-
-  const menu = [
-    { name: "Dashboard", icon: <Home set="two-tone" />, path: "/dashboard" },
-    {
-      name: "Master Data",
-      icon: <Folder set="two-tone" />,
-      submenu: [
-        { name: "Item 1", path: "/master-data/item1" },
-        { name: "Item 2", path: "/master-data/item2" },
-      ],
-      toggle: toggleMasterData,
-    },
-    {
-      name: "Resource Manager",
-      icon: <Document set="two-tone" />,
-      submenu: [
-        { name: "Translation", path: "/resource-manager/translation" },
-        { name: "Non Translation", path: "/resource-manager/non-translation" },
-        { name: "Vendor", path: "/resource-manager/vendor" },
-      ],
-      toggle: toggleResourceManager,
-    },
-    {
-      name: "System Administrator",
-      icon: <TwoUsers set="two-tone" />,
-      path: "/system-administrator",
-    },
-    { name: "Record Log", icon: <Activity set="two-tone" />, path: "/record-log" },
-  ];
-
-  const logOutItem = { name: "Log Out", icon: <Logout set="two-tone" />, path: "/logout" };
 
   const isActiveSubMenuParent = (submenu) => {
     return submenu.some((item) => location.pathname.includes(item.path));
@@ -58,9 +21,7 @@ export default function SideMenu() {
   };
 
   const handleNavigation = (path) => {
-    if (path) {
-      navigate(path);
-    }
+    if (path) navigate(path);
   };
 
   return (
@@ -69,55 +30,60 @@ export default function SideMenu() {
         <div>
           <div className="mb-6 text-gray-300 ms-4 font-medium text-xs">MAIN MENU</div>
           <ul className="space-y-7 mb-8">
-            {menu.map((val, index) => (
+            {menu.map((item, index) => (
               <React.Fragment key={`menu-${index}`}>
-                <li className={`flex flex-row items-center ${val.submenu ? "cursor-pointer" : ""}`} onClick={val.submenu ? val.toggle : () => handleNavigation(val.path)}>
+                <li onClick={() => (item.submenu ? toggleSubMenu(item.name) : handleNavigation(item.path))} className={`flex flex-row items-center ${item.submenu ? "cursor-pointer" : ""}`}>
                   <div className="ms-4 flex flex-row items-center w-full">
                     <div className="mr-2 w-6 h-6">
-                      {React.cloneElement(val.icon, {
-                        set: isActive(val.path, val.submenu) ? "bulk" : "two-tone",
-                        primaryColor: isActive(val.path, val.submenu) ? "#DC362E" : undefined,
+                      {React.cloneElement(item.icon, {
+                        set: isActive(item.path, item.submenu) ? "bulk" : "two-tone", // Change to 'bulk' when active
+                        primaryColor: isActive(item.path, item.submenu) ? "#DC362E" : undefined,
                       })}
                     </div>
-                    <div className="text-sm flex-grow" style={{ color: isActive(val.path, val.submenu) ? "#DC362E" : undefined }}>
-                      {val.name}
-                    </div>
-                    {val.submenu && (
-                      <div className="ml-auto mr-4">
-                        {(val.name === "Master Data" && masterDataOpen) || (val.name === "Resource Manager" && resourceManagerOpen) ? <ChevronUp size="small" stroke="light" /> : <ChevronDown size="small" stroke="light" />}
-                      </div>
-                    )}
+                    <div className={`text-sm flex-grow ${isActive(item.path, item.submenu) ? "text-[#DC362E]" : ""}`}>{item.name}</div>
+                    {item.submenu && <div className="ml-auto mr-4">{openSubMenus[item.name] ? <ChevronUp size="small" stroke="light" /> : <ChevronDown size="small" stroke="light" />}</div>}
                   </div>
                 </li>
-                {val.submenu && ((val.name === "Master Data" && masterDataOpen) || (val.name === "Resource Manager" && resourceManagerOpen)) && (
+                {item.submenu && openSubMenus[item.name] && (
                   <ul className="ml-12 space-y-6">
-                    {val.submenu.map((item, subIndex) => (
+                    {item.submenu.map((subItem, subIndex) => (
                       <li
                         key={`submenu-${subIndex}`}
-                        className="text-sm"
+                        className={`text-sm ${location.pathname.includes(subItem.path) ? "text-[#DC362E]" : ""}`}
                         onClick={(e) => {
                           e.stopPropagation();
-                          handleNavigation(item.path);
+                          handleNavigation(subItem.path);
                         }}
-                        style={{ color: location.pathname.includes(item.path) ? "#DC362E" : undefined }}
                       >
-                        {item.name}
+                        {subItem.name}
                       </li>
                     ))}
                   </ul>
                 )}
-                {val.name === "Record Log" && <div className="border-b-2 border-[#E9E9E9] w-full my-2"></div>}
+                {item.name === "Record Log" && <div className="border-b-2 border-[#E9E9E9] w-full my-2"></div>}
               </React.Fragment>
             ))}
-            <li className="flex flex-row items-center" onClick={() => handleNavigation(logOutItem.path)}>
-              <div className="ms-4 flex flex-row items-center w-full">
-                <div className="mr-2 w-6 h-6">{logOutItem.icon}</div>
-                <div className="text-sm">{logOutItem.name}</div>
-              </div>
-            </li>
           </ul>
         </div>
       </div>
     </div>
   );
 }
+
+SideMenu.propTypes = {
+  menu: PropTypes.arrayOf(
+    PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      icon: PropTypes.node.isRequired,
+      path: PropTypes.string,
+      submenu: PropTypes.arrayOf(
+        PropTypes.shape({
+          name: PropTypes.string.isRequired,
+          path: PropTypes.string.isRequired,
+        })
+      ),
+    })
+  ).isRequired,
+};
+
+export default SideMenu;
